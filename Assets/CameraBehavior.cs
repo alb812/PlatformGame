636577e8b31443ebff79512f113f74f13e5f0f60
Ariana B <alb886@nyu.edu>
2018-02-01 11:41:52 -0500

    Worked on Player and Camera Movement

---
+++ Assets/CameraBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public GameObject player;       //to insert the player gameObject
+
+	private Vector3 offset;         //Stores offset distance between the player and camera
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Calculates the distance between the player's position and camera's position as the offset.
+		offset = transform.position - player.transform.position;
+	}
+
+	// Its called after Update each frame
+	void LateUpdate () 
+	{
+		// Makes position of the camera the same as the player.
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/PlayerBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	//for basic player movement
+	public float speed = 2.0f;
+	private float jumpForce;
+	private Rigidbody2D rb;
+	private bool isJumping;
+
+	//so player only jumps once
+	/*private bool isTouchingGround;
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;*/
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		jumpForce = 500;
+		isJumping = false;
+
+	}
+
+	void Update ()
+	{
+		//So player can only jump if they're touching the ground
+		//isTouchingGround = Physics2D.OverlapCircle (groundCheckPoint.position, groundCheckRadius, groundLayer);
+
+		//Player movement left
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		//Player movement right
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		//Player movement for jump
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			rb.AddForce (new Vector2(0, jumpForce));
+		}
+	}
+
+	//check for Jump
+	void onCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground" /*&& isTouchingGround*/)
+			isJumping = false;
+	}
+}

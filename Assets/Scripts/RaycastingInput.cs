0af6ed708de76d08b07840c592058448e18f06fd
Ariana B <alb886@nyu.edu>
2018-02-15 09:36:48 -0500

    Raycasting and Destroy Items

---
+++ Assets/Scripts/RaycastingInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastingInput : MonoBehaviour {
+
+	SpriteRenderer TempEnemy;
+
+	// Use this for initialization
+	void Start () {
+		TempEnemy = GetComponent<SpriteRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit2D rayCastHit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+		if (rayCastHit.collider != null) {
+
+			//the ray has hit something with a collider on it
+			if (rayCastHit.collider.tag == "Enemy") { //check if name of collider matches what we're looking for
+				TempEnemy.color = Color.magenta; //we know its cat sprite, so change color 
+
+			} else {
+				TempEnemy.color = Color.white;
+			}
+
+
+		}else{ 
+			TempEnemy.color = Color.white;
+
+		}
+	}
+}
--- Assets/Scripts/TempItemBehavior.cs
+++ Assets/Scripts/TempItemBehavior.cs
@@ -10,9 +10,10 @@ public class TempItemBehavior : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-
+	void Update () 
+		{
+			//Rotate thet transform of the game object this is attached to by 45 degrees, taking into account the time elapsed since last frame.
+			transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+		}
 
 }

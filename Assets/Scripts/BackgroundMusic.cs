278c31993539fe348fe9b014b62fa625c5c5e659
Ariana B <alb886@nyu.edu>
2018-04-10 00:15:42 -0400

    Fixes

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+
+	public AudioSource BossMusica;
+	public AudioSource MainMusica;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other) 
+		{
+			//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+			if (other.gameObject.CompareTag("Player"))
+			{
+			MainMusica.Stop ();
+			BossMusica.Play ();
+			}
+	}
+}

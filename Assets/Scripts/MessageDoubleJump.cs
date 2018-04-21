fa21f9e08caf90b516f0b457ad823e4cb275b772
Ariana B <alb886@nyu.edu>
2018-04-20 21:34:29 -0400

    Re-Added Boss Music, Added More Directions
    Playtesting fixes

--- Assets/Scripts/MessageDoubleJump.cs
+++ Assets/Scripts/MessageDoubleJump.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 public class MessageDoubleJump : MonoBehaviour {
 
 	[Space(10)]
-	[Header("Toggle for the gui on off")]
+	[Header("Gui on off")]
 	public bool GuiOn;
 
 
 	[Space(10)]
-	[Header("The text to Display on Trigger")]
+	[Header("The text for Trigger")]
 
 	public string Text = "Jump";
 
@@ -57,13 +57,13 @@ public class MessageDoubleJump : MonoBehaviour {
 
 		if (GuiOn == true)
 		{
-			// Make a group on the center of the screen
+			// Makes a group at the center of screen
 			GUI.BeginGroup (new Rect ((Screen.width - BoxSize.width) / 2, (Screen.height - BoxSize.height) / 2, BoxSize.width, BoxSize.height));
-			// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
+			// Adjusts rectangles are to the group. 
 
 			GUI.Label(BoxSize, Text);
 
-			// End the group we started above. This is very important to remember!
+			// Ends the group
 			GUI.EndGroup ();
 
 		}
